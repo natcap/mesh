6b6d3446a2f451c20159b6748a60d72533e6445d
James Douglass <jdouglass@stanford.edu>
2015-10-27 12:40:33 -0700

    Removing temporary directories before building MESH and zipping binaries

--- pavement.py
+++ pavement.py
@@ -1,3 +1,5 @@
+import shutil
+import os
 
 import paver.easy
 import paver.virtual
@@ -17,10 +19,22 @@ def build_bin():
     """
     Build the MESH Pyinstaller binaries.
     """
+    for dirname in ['dist', 'build']:
+        if os.path.exists(dirname):
+            shutil.rmtree(dirname)
+
+    # Build the MESH pyinstaller binaries.
     @paver.virtual.virtualenv(paver.easy.options.virtualenv.dest_dir)
     def _build_bin():
         paver.easy.sh('pyinstaller exe/mesh.spec')
     _build_bin()
+
+    # Zip up the binaries.
+    shutil.make_archive(**{
+        'base_name': 'mesh_binaries',
+        'format': 'zip',
+        'root_dir': 'dist/mesh_dist',
+        'base_dir': 'mesh_binaries'})
 
 @paver.easy.task
 def env(options):
