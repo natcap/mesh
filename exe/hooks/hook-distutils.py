d5a18c4428aeff6fbada82b92c15dc30d25f6f94
James Douglass <jdouglass@stanford.edu>
2015-10-27 14:41:16 -0700

    Copying the distutils hook from http://code.openhub.net/file?fid=itEgTor95jegtClNpJcNjIW-y0M&cid=uHeRsSq1A_U&s=/import%20django/%20lang:ObjectiveC&fp=475133&mp&projSelected=true#L0

--- exe/hooks/hook-distutils.py
+++ exe/hooks/hook-distutils.py
@@ -1,13 +1,56 @@
-from PyInstaller.hooks.hookutils import collect_submodules
-hiddenimports = collect_submodules('distutils')
 
-# From https://github.com/kennethreitz-archive/pyinstaller/blob/master/hooks/hook-distutils.py
+import distutils
+import distutils.sysconfig
+import marshal
+import os
+import sys
+
+from PyInstaller import compat
+from PyInstaller.compat import is_win
+
+
+# distutils module requires Makefile and pyconfig.h files from Python
+# installation. 'distutils.sysconfig' parses these files to get some
+# information from them.
+_CONFIG_H = distutils.sysconfig.get_config_h_filename()
+_MAKEFILE = distutils.sysconfig.get_makefile_filename()
+
+
+# In virtualenv sys.prefix is overridden.
+if hasattr(sys, 'real_prefix'):
+    sys_prefix = sys.real_prefix
+else:
+    sys_prefix = sys.prefix
+
+
+# Relative path to config_h in the dist directory.
+_frozen_config_h = compat.relpath(os.path.dirname(_CONFIG_H), sys_prefix)
+# Data files in PyInstaller hook format.
+datas = [
+    (_CONFIG_H, _frozen_config_h),
+]
+
+
+# On Windows Makefile does not exist.
+if not is_win:
+    _frozen_makefile = compat.relpath(os.path.dirname(_MAKEFILE), sys_prefix)
+    datas.append((_MAKEFILE, _frozen_makefile))
+
+
 def hook(mod):
-    import distutils
-    if hasattr(distutils, "distutils_path"):
-        import os
-        import marshal
-        mod_path = os.path.join(distutils.distutils_path, "__init__.pyc")
-        parsed_code = marshal.loads(open(mod_path, "rb").read()[8:])
+    """
+    Contributed by jkp@kirkconsulting.co.uk
+    This hook checks for the distutils hacks present when using the
+    virtualenv package.
+    """
+    # Non-empty  means PyInstaller is running inside virtualenv.
+    # Virtualenv overrides real distutils modules.
+    if hasattr(distutils, 'distutils_path'):
+        mod_path = os.path.join(distutils.distutils_path, '__init__.pyc')
+        try:
+            parsed_code = marshal.loads(open(mod_path, 'rb').read()[8:])
+        except IOError:
+            parsed_code = compile(open(mod_path[:-1], 'rU').read(), mod_path, 'exec')
         mod.__init__('distutils', mod_path, parsed_code)
     return mod
+
